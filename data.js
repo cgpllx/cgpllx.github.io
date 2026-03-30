3ae63aa1872ff0fa5ff643c9c15c36aa3cce9c1c
耳东 <cgpllx1@qq.com>
2026-03-30 17:13:15 +0800

    vr
    vr

---
+++ data.js
@@ -0,0 +1,222 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "ddd",
+      "name": "location_01",
+      
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.930683952566664,
+        "pitch": 0.3456887542822109,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4714795904280606,
+          "pitch": 0.3572729455486421,
+          "rotation": 0,
+          "target": "1-location_02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.783401751538573,
+          "pitch": 0.08791089411651853,
+          "title": "白色大冰箱",
+          "text": "冰箱"
+        }
+      ]
+    },{
+      "id": "location_01",
+      "name": "location_01",
+      
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.930683952566664,
+        "pitch": 0.3456887542822109,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4714795904280606,
+          "pitch": 0.3572729455486421,
+          "rotation": 0,
+          "target": "1-location_02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.783401751538573,
+          "pitch": 0.08791089411651853,
+          "title": "白色大冰箱",
+          "text": "冰箱"
+        }
+      ]
+    },
+    {
+      "id": "1-location_02",
+      "name": "location_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.8705080596643135,
+        "pitch": 0.19928183658807797,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.007812175294800383,
+          "pitch": 0.3518938993803893,
+          "rotation": 0,
+          "target": "0-location_01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-location_03",
+      "name": "location_03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-location_04",
+      "name": "location_04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
